9da570ab10775aa65052ca7ecdf86eb28bd57166
TrinhDuc01 <ductrinh.0301@gmail.com>
2024-08-14 04:51:09 +0700

    format .sh

--- rental-bike/bike-shop.sh
+++ rental-bike/bike-shop.sh
@@ -5,20 +5,19 @@ PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
 MAIN_MENU() {
-  if [[ $1 ]]
-  then
+  if [[ $1 ]]; then
     echo -e "\n$1"
   fi
 
-  echo "How may I help you?" 
+  echo "How may I help you?"
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
 
   case $MAIN_MENU_SELECTION in
-    1) RENT_MENU ;;
-    2) RETURN_MENU ;;
-    3) EXIT ;;
-    *) MAIN_MENU "Please enter a valid option." ;;
+  1) RENT_MENU ;;
+  2) RETURN_MENU ;;
+  3) EXIT ;;
+  *) MAIN_MENU "Please enter a valid option." ;;
   esac
 }
 
@@ -27,15 +26,13 @@ RENT_MENU() {
   AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id")
 
   # if no bikes available
-  if [[ -z $AVAILABLE_BIKES ]]
-  then
+  if [[ -z $AVAILABLE_BIKES ]]; then
     # send to main menu
     MAIN_MENU "Sorry, we don't have any bikes available right now."
   else
     # display available bikes
     echo -e "\nHere are the bikes we have available:"
-    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
-    do
+    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE; do
       echo "$BIKE_ID) $SIZE\" $TYPE Bike"
     done
 
@@ -44,8 +41,7 @@ RENT_MENU() {
     read BIKE_ID_TO_RENT
 
     # if input is not a number
-    if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
-    then
+    if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]; then
       # send to main menu
       MAIN_MENU "That is not a valid bike number."
     else
@@ -53,8 +49,7 @@ RENT_MENU() {
       BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT AND available = true")
 
       # if not available
-      if [[ -z $BIKE_AVAILABILITY ]]
-      then
+      if [[ -z $BIKE_AVAILABILITY ]]; then
         # send to main menu
         MAIN_MENU "That bike is not available."
       else
@@ -63,13 +58,12 @@ RENT_MENU() {
         read PHONE_NUMBER
         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
         # if customer doesn't exist
-        if [[ -z $CUSTOMER_NAME ]]
-        then
-        # get new customer name
-        echo -e "\nWhat's your name?"
-        read CUSTOMER_NAME
-        # insert new customer
-        INSERT_CUSTOMER_RESULT=$($PSQL "insert into customers(phone,name) values('$PHONE_NUMBER','$CUSTOMER_NAME')")
+        if [[ -z $CUSTOMER_NAME ]]; then
+          # get new customer name
+          echo -e "\nWhat's your name?"
+          read CUSTOMER_NAME
+          # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "insert into customers(phone,name) values('$PHONE_NUMBER','$CUSTOMER_NAME')")
         fi
         # get customer_id
         CUSTOMER_ID=$($PSQL "select customer_id from customers where phone='$PHONE_NUMBER'")
@@ -93,51 +87,46 @@ RETURN_MENU() {
   read PHONE_NUMBER
   CUSTOMER_ID=$($PSQL "select customer_id from customers where phone='$PHONE_NUMBER'")
   # if not found
-  if [[ -z $CUSTOMER_ID ]]
-  then
-  # send to main menu
-  MAIN_MENU "I could not find a record for that phone number."
-  else
-  # get customer's rentals
-  CUSTOMER_RENTALS=$($PSQL "SELECT bike_id, type, size FROM bikes INNER JOIN rentals USING(bike_id) INNER JOIN customers USING(customer_id) WHERE phone = '$PHONE_NUMBER' AND date_returned IS NULL order by bike_id;")
-  # if no rentals
-  if [[ -z $CUSTOMER_RENTALS ]]
-  then
-  # send to main menu
-  MAIN_MENU "You do not have any bikes rented."
-  else
-  # display rented bikes
-  echo -e "\nHere are your rentals:"
-  echo "$CUSTOMER_RENTALS" | while read BIKE_ID BAR TYPE BAR SIZE
-  do
-    echo "$BIKE_ID) $SIZE\" $TYPE Bike"
-  done
-  # ask for bike to return
-  echo -e "\nWhich one would you like to return?"
-  read BIKE_ID_TO_RETURN
-  # if not a number
-  if [[ ! $BIKE_ID_TO_RETURN =~ ^[0-9]+$ ]]
-  then
-  # send to main menu
-  MAIN_MENU "That is not a valid bike number."
-  else
-  # check if input is rented
-  RENTAL_ID=$($PSQL "select rental_id from rentals inner join customers using(customer_id) where phone='$PHONE_NUMBER' and bike_id=$BIKE_ID_TO_RETURN and date_returned is null")
-  # if input not rented
-  if [[ -z $RENTAL_ID ]]
-  then
-  # send to main menu
-  MAIN_MENU "You do not have that bike rented."
-  else
-    # update date_returned
-    RETURN_BIKE_RESULT=$($PSQL "update rentals set date_returned = NOW() where rental_id=$RENTAL_ID")
-    # set bike availability to true
-    SET_TO_TRUE_RESULT=$($PSQL  "update bikes set available=TRUE where bike_id=$BIKE_ID_TO_RETURN")
+  if [[ -z $CUSTOMER_ID ]]; then
     # send to main menu
-    MAIN_MENU "Thank you for returning your bike."
-  fi
-  fi
-  fi
+    MAIN_MENU "I could not find a record for that phone number."
+  else
+    # get customer's rentals
+    CUSTOMER_RENTALS=$($PSQL "SELECT bike_id, type, size FROM bikes INNER JOIN rentals USING(bike_id) INNER JOIN customers USING(customer_id) WHERE phone = '$PHONE_NUMBER' AND date_returned IS NULL order by bike_id;")
+    # if no rentals
+    if [[ -z $CUSTOMER_RENTALS ]]; then
+      # send to main menu
+      MAIN_MENU "You do not have any bikes rented."
+    else
+      # display rented bikes
+      echo -e "\nHere are your rentals:"
+      echo "$CUSTOMER_RENTALS" | while read BIKE_ID BAR TYPE BAR SIZE; do
+        echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+      done
+      # ask for bike to return
+      echo -e "\nWhich one would you like to return?"
+      read BIKE_ID_TO_RETURN
+      # if not a number
+      if [[ ! $BIKE_ID_TO_RETURN =~ ^[0-9]+$ ]]; then
+        # send to main menu
+        MAIN_MENU "That is not a valid bike number."
+      else
+        # check if input is rented
+        RENTAL_ID=$($PSQL "select rental_id from rentals inner join customers using(customer_id) where phone='$PHONE_NUMBER' and bike_id=$BIKE_ID_TO_RETURN and date_returned is null")
+        # if input not rented
+        if [[ -z $RENTAL_ID ]]; then
+          # send to main menu
+          MAIN_MENU "You do not have that bike rented."
+        else
+          # update date_returned
+          RETURN_BIKE_RESULT=$($PSQL "update rentals set date_returned = NOW() where rental_id=$RENTAL_ID")
+          # set bike availability to true
+          SET_TO_TRUE_RESULT=$($PSQL "update bikes set available=TRUE where bike_id=$BIKE_ID_TO_RETURN")
+          # send to main menu
+          MAIN_MENU "Thank you for returning your bike."
+        fi
+      fi
+    fi
   fi
 }
 
--- worldcup/insert_data.sh
+++ worldcup/insert_data.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-if [[ $1 == "test" ]]
-then
+if [[ $1 == "test" ]]; then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
@@ -10,31 +9,24 @@ fi
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
 echo "$($PSQL "TRUNCATE TABLE games, teams;")"
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNERGOALS OPPONENTGOALS
-do
-  if [[ $YEAR != 'year' ]]
-  then
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNERGOALS OPPONENTGOALS; do
+  if [[ $YEAR != 'year' ]]; then
     IDWINNER="$($PSQL "select team_id from teams where name='$WINNER'")"
     IDOPPONENT="$($PSQL "select team_id from teams where name='$OPPONENT'")"
-    if [[ -z $IDWINNER ]]
-    then
+    if [[ -z $IDWINNER ]]; then
       ADDWINNER="$($PSQL "insert into teams(name) values('$WINNER')")"
     fi
-    if [[ -z $IDOPPONENT ]]
-    then
+    if [[ -z $IDOPPONENT ]]; then
       ADDOPPONENT="$($PSQL "insert into teams(name) values('$OPPONENT')")"
     fi
   fi
 done
 
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNERGOALS OPPONENTGOALS
-do
-  if [[ $YEAR != 'year' ]]
-  then
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNERGOALS OPPONENTGOALS; do
+  if [[ $YEAR != 'year' ]]; then
     IDWINNER="$($PSQL "select team_id from teams where name='$WINNER'")"
     IDOPPONENT="$($PSQL "select team_id from teams where name='$OPPONENT'")"
-    if [[ $IDWINNER > 1 ]]
-    then
+    if [[ $IDWINNER > 1 ]]; then
       ADDGAME="$($PSQL "insert into games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) values($YEAR,'$ROUND',$IDWINNER,$IDOPPONENT,$WINNERGOALS,$OPPONENTGOALS)")"
     fi
   fi
